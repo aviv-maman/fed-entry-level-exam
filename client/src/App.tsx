955a3a039801876fc1a97a90f5ce34708d1a9077
aviv-maman <awiwmaman@outlook.com>
2022-11-04 15:58:10 +0200

    part 1a - show content of tickets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,44 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import { Tickets } from './Tickets';
 import { createApiClient, Ticket } from './api';
 
 export type AppState = {
-	tickets?: Ticket[],
-	search: string;
-}
+  tickets?: Ticket[];
+  search: string;
+};
 
 const api = createApiClient();
+
 const App = () => {
-	const [search, setSearch] =
-		React.useState<string>('');
-	const [tickets, setTickets] =
-		React.useState<Ticket[]>([]);
-	React.useEffect(() => {
-		async function fetchTickets() {
-			setTickets(await api.getTickets())
-		}
-		fetchTickets()
-	}, []);
-	let searchDebounce: any;
-	const onSearch = (val: string, newPage?: number) => {
-		clearTimeout(searchDebounce);
-		searchDebounce = setTimeout(async () => {
-			setSearch(val)
-		}, 300);
-	}
-	return (
-		<main>
-			<h1>Tickets List</h1>
-			<header>
-				<input type="search" placeholder="Search..." onChange={(e) => onSearch(e.target.value)} />
-			</header>
-			{tickets ? <div className='results'>Showing {tickets.length} results</div> : null}
-			{tickets ? <Tickets tickets={tickets} search={search} /> : <h2>Loading..</h2>}
-		</main>);
-}
+  const [search, setSearch] = useState<string>('');
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    async function fetchTickets() {
+      setTickets(await api.getTickets());
+    }
+    fetchTickets();
+  }, []);
+
+  let searchDebounce: any;
+  const onSearch = (val: string, newPage?: number) => {
+    clearTimeout(searchDebounce);
+    searchDebounce = setTimeout(async () => {
+      setSearch(val);
+    }, 300);
+  };
+
+  return (
+    <main>
+      <h1>Tickets List</h1>
+      <header>
+        <input type='search' placeholder='Search...' onChange={(e) => onSearch(e.target.value)} />
+      </header>
+      {tickets ? <div className='results'>Showing {tickets.length} results</div> : null}
+      {tickets ? <Tickets tickets={tickets} search={search} /> : <h2>Loading..</h2>}
+    </main>
+  );
+};
+
 export default App;
